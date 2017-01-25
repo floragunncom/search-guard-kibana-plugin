e5cb6faeeb7ecbc40b5dd46ed40726515ba60077
Jochen Kressin <jkressin@floragunn.com>
2017-01-25 15:51:37 +0100

    added config key to disable basic auth

--- index.js
+++ index.js
@@ -12,21 +12,27 @@ export default function (kibana) {
         name: 'searchguard',
         require: ['kibana', 'elasticsearch'],
 
-        // config validation
-        config(Joi) {
-            return Joi.object({
+        config: function (Joi) {
+            var obj = Joi.object({
                 enabled: Joi.boolean().default(true),
+                cookie: Joi.object().keys({
+                    secure: Joi.boolean().default(true),
+                    name: Joi.string().default('searchguard_authentication'),
+                    password: Joi.string().min(32).required(),
+                    ttl: Joi.number().integer().min(0).allow(null)
+                }).default(),
                 session: Joi.object().keys({
                     ttl: Joi.number().integer().min(1).default(60 * 60 * 1000),
-                    keepalive: Joi.boolean().default(true)
-                }),
-                cookie: Joi.object().keys({
-                    password: Joi.string(), // gets validated manually
-                    secure: Joi.boolean().default(false),
-                    name: Joi.string().default('searchguard_authentication'),
-                    ttl: Joi.number().integer().min(0).allow(null),
-                })
+                    keepalive: Joi.boolean().default(true),
+                }).default(),
+                basicauth: Joi.object().keys({
+                    enabled: Joi.boolean().default(true)
+                }).default(),
+                multitenancy: Joi.object().keys({
+                    bla: Joi.string().default('blub')
+                }).default()
             }).default();
+            return obj;
         },
 
         uiExports: {
@@ -50,43 +56,50 @@ export default function (kibana) {
 
             APP_ROOT = '/searchguard';
             API_ROOT = `${APP_ROOT}/api`;
-
+            console.log("Calling");
             const config = server.config();
-
             // validate config, only mandatory field is password
             if(config.get('searchguard.cookie.password') == null) {
                 this.status.red('searchguard.cookie.password not set in kibana.yml, please specify this value (min. 32 characters)');
                 throw new Error('+++ Search Guard authentication not available +++');
             }
 
-            server.register([
-                require('hapi-async-handler'),
-                require('hapi-auth-cookie'),
-                require('hapi-authorization')
-            ], (error) => {
-                if (error) {
-                    server.log(['error', 'searchguard'], `An error occurred registering server plugins: ${error}`);
-                    this.status.red('An error occurred during initialisation, please check the logs.');
-                    return;
-                }
+            if(config.get('searchguard.basicauth.enabled')) {
+                server.register([
+                    require('hapi-async-handler'),
+                    require('hapi-auth-cookie'),
+                    require('hapi-authorization')
+                ], (error) => {
+                    if (error) {
+                        server.log(['error', 'searchguard'], `An error occurred registering server plugins: ${error}`);
+                        this.status.red('An error occurred during initialisation, please check the logs.');
+                        return;
+                    }
 
-                this.status.yellow('Initialising Search Guard authentication plugin.');
+                    this.status.yellow('Initialising Search Guard authentication plugin.');
 
-                // provides authentication methods against Search Guard
-                const BackendClass = pluginRoot(`lib/backend/searchguard`);
-                const authenticationBackend = new BackendClass(server, server.config);
+                    // provides authentication methods against Search Guard
+                    const BackendClass = pluginRoot(`lib/backend/searchguard`);
+                    const authenticationBackend = new BackendClass(server, server.config);
 
-                // we use the cookie strategy
-                require('./lib/hapi/auth')(pluginRoot, server, APP_ROOT, API_ROOT);
+                    // we use the cookie strategy
+                    require('./lib/hapi/auth')(pluginRoot, server, APP_ROOT, API_ROOT);
 
-                // all your routes are belong to us
-                require('./lib/routes/routes')(pluginRoot, server, this, APP_ROOT, API_ROOT);
+                    // all your routes are belong to us
+                    require('./lib/routes/routes')(pluginRoot, server, this, APP_ROOT, API_ROOT);
 
-                // make auth backend available
-                server.expose('getAuthenticationBackend', () => authenticationBackend);
+                    // make auth backend available
+                    server.expose('getAuthenticationBackend', () => authenticationBackend);
 
-                this.status.green('Search Guard access control initialised.');
-            });
+                    this.status.yellow('Search Guard HTTP Basic Authentication enabled.');
+
+                });
+
+            } else {
+                this.status.yellow('Search Guard HTTP Basic Authentication is disabled.');
+            }
+
+            this.status.green('Search Guard plugin initialised.');
 
         }
 
