903c4f8a950e3fe80274052b24f7f26a0fdff016
Jochen Kressin <jkressin@floragunn.com>
2017-02-04 22:06:13 +0100

    harmonize build script

--- build.sh
+++ build.sh
@@ -1,28 +1,19 @@
 #!/bin/bash
-set -e
-LOGILE=build.log
-echo "$(date)" > $LOGILE
-PLUGIN_NAME=searchguard-kibana-alpha
+PLUGIN_NAME=searchguard-kibana
 PLUGIN_VERSION=5.1.2
 echo "Building $PLUGIN_NAME-$PLUGIN_VERSION.zip"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-if [ ! -d "$DIR/../kibana/.git" ]; then
-    cd $DIR/..
-    git clone https://github.com/elastic/kibana.git >> $LOGILE 2>&1
-else
-    cd "$DIR/../kibana"
-    git fetch >> $LOGILE 2>&1
-fi
-git checkout tags/v$PLUGIN_VERSION >> $LOGILE 2>&1
-hash nvm 2>/dev/null || export NVM_DIR=~/.nvm; mkdir -p $NVM_DIR; . $(brew --prefix nvm)/nvm.sh >> $LOGILE 2>&1
-nvm install "$(cat .node-version)" >> $LOGILE 2>&1
+cd $DIR/..
+git clone https://github.com/elastic/kibana.git
+cd "kibana"
+git checkout tags/v$PLUGIN_VERSION
+hash nvm 2>/dev/null || export NVM_DIR=~/.nvm; mkdir -p $NVM_DIR; . $(brew --prefix nvm)/nvm.sh
+nvm install "$(cat .node-version)"
 cd "$DIR"
 rm -rf build/
-rm -rf $DIR/releases
 rm -rf node_modules/
-npm install --save hapi@16.0.1 >> $LOGILE 2>&1
-npm install >> $LOGILE 2>&1
+npm install --save hapi@16.0.1
+npm install
 COPYPATH="build/kibana/$PLUGIN_NAME"
 mkdir -p $COPYPATH
 cp -a index.js $COPYPATH
@@ -33,9 +24,9 @@ cp -a public $COPYPATH
 #cp -a server $COPYPATH
 cd build
 zip --quiet -r $PLUGIN_NAME-$PLUGIN_VERSION.zip kibana
-#ls -lah $PLUGIN_NAME-$PLUGIN_VERSION.zip
+ls -lah $PLUGIN_NAME-$PLUGIN_VERSION.zip
 cd $DIR
-mkdir -p $DIR/releases/$PLUGIN_VERSION/
-cp $DIR/build/$PLUGIN_NAME-$PLUGIN_VERSION.zip $DIR/releases/$PLUGIN_VERSION/
-echo "Created $DIR/releases/$PLUGIN_VERSION/$PLUGIN_NAME-$PLUGIN_VERSION.zip"
-md5sum $DIR/releases/$PLUGIN_VERSION/$PLUGIN_NAME-$PLUGIN_VERSION.zip
+mkdir -p releases/$PLUGIN_VERSION/
+cp build/$PLUGIN_NAME-$PLUGIN_VERSION.zip releases/$PLUGIN_VERSION/
+echo "Created releases/$PLUGIN_VERSION/$PLUGIN_NAME-$PLUGIN_VERSION.zip"
+md5sum build/$PLUGIN_NAME-$PLUGIN_VERSION.zip
