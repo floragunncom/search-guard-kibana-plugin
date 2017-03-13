80da3c502134b48c65205bfa7125e43257532f1d
Jochen Kressin <jkressin@floragunn.com>
2017-03-13 20:55:01 +0100

    prepare for beta2

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 PLUGIN_NAME=searchguard-kibana
-PLUGIN_VERSION=5.2.2-SNAPSHOT
+PLUGIN_VERSION=5.2.2-beta2
 echo "Building $PLUGIN_NAME-$PLUGIN_VERSION.zip"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/..
