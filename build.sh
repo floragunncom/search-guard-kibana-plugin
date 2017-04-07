0b7ff6f4325994ad70ad46124ad2d4f02006b9fe
Jochen Kressin <jkressin@floragunn.com>
2017-04-07 14:40:14 +0200

    bumped version number

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 PLUGIN_NAME=searchguard-kibana
-PLUGIN_VERSION=5.2.2-beta3
+PLUGIN_VERSION=5.2.2-beta3.1
 KIBANA_VERSION=5.2.2
 echo "Building $PLUGIN_NAME-$PLUGIN_VERSION.zip"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
