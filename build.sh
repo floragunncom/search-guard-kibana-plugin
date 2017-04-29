1d1c975d475c1a4844f504f85bf0d5441deb1279
Jochen Kressin <jkressin@floragunn.com>
2017-04-29 12:08:43 +0200

    prepare release 4.6.0-2

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 PLUGIN_NAME=searchguard-kibana
-PLUGIN_VERSION=4.6.0-1
+PLUGIN_VERSION=4.6.0-2
 echo "Building $PLUGIN_NAME-$PLUGIN_VERSION.zip"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/..
