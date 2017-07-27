19ba2d9c4e0348939a6b3a1d119fd277bfa9fa8e
Jochen Kressin <jkressin@floragunn.com>
2017-07-27 23:10:51 +0200

    prepare 5.5.1

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PLUGIN_NAME=searchguard-kibana
-PLUGIN_VERSION=5.5.0-3
-KIBANA_VERSION=5.5.0
+PLUGIN_VERSION=5.5.1-3
+KIBANA_VERSION=5.5.1
 echo "Building $PLUGIN_NAME-$PLUGIN_VERSION.zip"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/..
