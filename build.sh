eff7ad6cd2623870b05633eb9e76081995250af6
Jochen Kressin <jkressin@floragunn.com>
2017-10-01 22:48:06 +0200

    bump to rc1, exclude systeminfo from authenticated routes

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PLUGIN_NAME=searchguard-kibana
-PLUGIN_VERSION=6.0.0-beta2
-KIBANA_VERSION=6.0.0-beta2
+PLUGIN_VERSION=6.0.0-rc1
+KIBANA_VERSION=6.0.0-rc1
 echo "Building $PLUGIN_NAME-$PLUGIN_VERSION.zip"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/..
