6ae49bf11e3cfa7ed564c278586069643a01f041
Jochen Kressin <jkressin@floragunn.com>
2017-10-08 22:12:17 +0200

    SG-747 select advanced mode automatically

--- public/apps/configuration/directives/permissions/permissions.js
+++ public/apps/configuration/directives/permissions/permissions.js
@@ -7,9 +7,24 @@ app.directive('sgcPermissions', function () {
     return {
         template: require('./permissions.html'),
         restrict: 'EA',
+        scope: true,
         scope: {
             "permissionsResource": "=permissionsresource",
         },
-        controller: 'sgBaseController'
+        controller: 'sgBaseController',
+        link: function(scope, elem, attr) {
+
+            scope.showAdvanced = null;
+
+            scope.$watch('permissionsResource', function(newValue, oldValue){
+                if(newValue && (scope.showAdvanced == null)) {
+                    if (scope.permissionsResource.permissions && scope.permissionsResource.permissions.length > 0) {
+                        scope.showAdvanced = true;
+                    }
+                }
+            }, true)
+
+
+        }
     }
 });
