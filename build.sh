56eeb9f3bdd49e7c08af053dcd48dac94bf8e764
Jochen Kressin <jkressin@floragunn.com>
2017-11-14 18:05:07 +0100

    Squashed commit of the following:
    commit dfc5cfb0cac83e19ccf4f9d1b725fa204ed81333
    Author: floragunn <dev@search-guard.com>
    Date:   Fri Oct 20 23:23:49 2017 +0200
    
        make a snapshot
    
    commit 896fca636642242209f47fdacbd75ac75d049a54
    Author: floragunn <dev@search-guard.com>
    Date:   Fri Oct 20 23:20:14 2017 +0200
    
        jdk image
    
    commit 0a56d508699353fc1e9aa92e492698d5a40683b2
    Author: floragunn <dev@search-guard.com>
    Date:   Fri Oct 20 23:18:52 2017 +0200
    
        trigger
    
    commit 2e49ca8ff62380f6b03d3acae06182f09f4110d1
    Author: floragunn <dev@search-guard.com>
    Date:   Fri Oct 20 23:15:25 2017 +0200
    
        add cirecleci support
    
    commit 91ddaa5a9f973f9c2397f490907def388d8a2035
    Author: floragunn <dev@search-guard.com>
    Date:   Thu Oct 19 19:37:13 2017 +0200
    
        make ready to deploy to maven

--- build.sh
+++ build.sh
@@ -1,33 +1,37 @@
 #!/bin/bash
 PLUGIN_NAME=searchguard-kibana
-PLUGIN_VERSION=6.0.0-rc1
-KIBANA_VERSION=6.0.0-rc1
-echo "Building $PLUGIN_NAME-$PLUGIN_VERSION.zip"
+KIBANA_VERSION="$1"
+echo "Building $PLUGIN_NAME-$KIBANA_VERSION.zip"
+set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $DIR/..
-git clone https://github.com/elastic/kibana.git
+cd "$DIR"
+mkdir -p build_stage
+cd build_stage
+git clone https://github.com/elastic/kibana.git || true
 cd "kibana"
-git checkout tags/v$KIBANA_VERSION
-hash nvm 2>/dev/null || export NVM_DIR=~/.nvm; mkdir -p $NVM_DIR; . $(brew --prefix nvm)/nvm.sh
+git fetch
+git checkout "tags/v$KIBANA_VERSION"
+hash nvm 2>/dev/null || export NVM_DIR=~/.nvm; mkdir -p "$NVM_DIR"; . $(brew --prefix nvm)/nvm.sh
 nvm install "$(cat .node-version)"
 cd "$DIR"
 rm -rf build/
 rm -rf node_modules/
-npm install --save hapi@16.0.1
+#npm install --save hapi@16.0.1
 npm install
 COPYPATH="build/kibana/$PLUGIN_NAME"
-mkdir -p $COPYPATH
-cp -a index.js $COPYPATH
-cp -a package.json $COPYPATH
-cp -a lib $COPYPATH
-cp -a node_modules $COPYPATH
-cp -a public $COPYPATH
-#cp -a server $COPYPATH
-cd build
-zip --quiet -r $PLUGIN_NAME-$PLUGIN_VERSION.zip kibana
-ls -lah $PLUGIN_NAME-$PLUGIN_VERSION.zip
-cd $DIR
-mkdir -p releases/$PLUGIN_VERSION/
-cp build/$PLUGIN_NAME-$PLUGIN_VERSION.zip releases/$PLUGIN_VERSION/
-echo "Created releases/$PLUGIN_VERSION/$PLUGIN_NAME-$PLUGIN_VERSION.zip"
-md5sum build/$PLUGIN_NAME-$PLUGIN_VERSION.zip
+mkdir -p "$COPYPATH"
+cp -a "$DIR/index.js" "$COPYPATH"
+cp -a "$DIR/package.json" "$COPYPATH"
+cp -a "$DIR/lib" "$COPYPATH"
+cp -a "$DIR/node_modules" "$COPYPATH"
+cp -a "$DIR/public" "$COPYPATH"
+
+#cd build
+#zip --quiet -r $PLUGIN_NAME-$PLUGIN_VERSION.zip kibana
+#ls -lah $PLUGIN_NAME-$PLUGIN_VERSION.zip
+#cd $DIR
+#mkdir -p releases/$PLUGIN_VERSION/
+#rm -f releases/$PLUGIN_VERSION/$PLUGIN_NAME-$PLUGIN_VERSION.zip
+#cp build/$PLUGIN_NAME-$PLUGIN_VERSION.zip releases/$PLUGIN_VERSION/
+#echo "Created releases/$PLUGIN_VERSION/$PLUGIN_NAME-$PLUGIN_VERSION.zip"
+#md5sum build/$PLUGIN_NAME-$PLUGIN_VERSION.zip
