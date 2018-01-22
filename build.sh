3e5a1bb4341dd7b6ae3c7f37d5c325b427022fd8
Jochen Kressin <jkressin@floragunn.com>
2018-01-22 21:57:47 +0100

    bump to 5.6.6

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PLUGIN_NAME=searchguard-kibana
-PLUGIN_VERSION=5.6.5-5
-KIBANA_VERSION=5.6.5
+PLUGIN_VERSION=5.6.6-5
+KIBANA_VERSION=5.6.6
 echo "Building $PLUGIN_NAME-$PLUGIN_VERSION.zip"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/..
