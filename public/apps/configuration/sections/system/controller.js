9682ee24876c479951862691e69c558155c472bb
Mikael Gustavsson <micael.gustafsson@gmail.com>
2018-02-04 15:33:53 +0100

    ITT-1024 - Upload license file

--- public/apps/configuration/sections/system/controller.js
+++ public/apps/configuration/sections/system/controller.js
@@ -43,10 +43,72 @@ app.controller('sgLicenseController', function ($scope, $element, $route, $locat
     var APP_ROOT = `${chrome.getBasePath()}`;
     var API_ROOT = `${APP_ROOT}/api/v1`;
     let notify = new Notifier({});
+    const form = $element.find('form[name="objectForm"]');
+    const fileField = $element.find('#js-uploadLicenseFile')[0];
+    /**
+     * Sanity check for the uploaded license file - maximum file size in bytes.
+     * @type {number}
+     */
+    const licenseFileLimit = 5000;
+
 
     $scope.title = function () {
         return "Upload new license";
+    };
+
+    /**
+     * Handle an uploaded license file
+     * @param event
+     */
+    function uploadLicenseFile(event) {
+
+        if (! event.target.files.length) {
+            return;
+        }
+
+        const allowedFileExtensions = ['.txt', '.lic'];
+
+        let file = event.target.files[0];
+        let fileNameParts = file.name.split('.');
+        // We only allow .txt and .lic in the html, but let's do a simple check here too
+        let fileExtension = '.' + fileNameParts[fileNameParts.length - 1];
+
+        if (allowedFileExtensions.indexOf(fileExtension) === -1) {
+            notify.error('Please select another file. We support the following file extensions: ' + allowedFileExtensions.join(', '));
+
+            return;
+        }
+
+        if (file.size > licenseFileLimit) {
+            notify.error('The file is larger than a typical license file. Please paste the content of the file into the text field.');
+
+            return;
+        }
+
+        let fileReader = new FileReader();
+
+        /**
+         * Triggered after the FileReader has read the file
+         * @param event
+         */
+        fileReader.onload = function(event) {
+            if (event.target.result) {
+                $scope.resource.licenseString = event.target.result;
+                $scope.$apply('resource');
+                form.submit();
+            }
+        };
+
+        fileReader.onerror = function (event) {
+            notify.error('Something went wrong with the uploaded file. Please paste the content of the file into the text field.');
+        };
+
+        fileReader.readAsText(file);
     }
+
+    // Angular doesn't handle the ng-change event on input type="file"
+    fileField.addEventListener('change', uploadLicenseFile);
+
 
     $scope.saveObject = (event) => {
 
@@ -82,6 +144,13 @@ app.controller('sgLicenseController', function ($scope, $element, $route, $locat
                 notify.error(error);
             });
     };
+
+    /**
+     * Clean up when the $scope is destroyed
+     */
+    $scope.$on('$destroy', function () {
+        fileField.removeEventListener('change', uploadLicenseFile);
+    });
 });
 
 
