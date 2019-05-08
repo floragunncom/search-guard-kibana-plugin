754f0fa648bd368d1495b3bda1dfedbe4af704c0
Sergii Bondarenko <sergibondarenko@gmail.com>
2019-05-08 19:12:48 +0200

    There is no need to uniq the roles because EuiComboBox already does it.

--- public/apps/configuration-react/pages/InternalUsers/containers/InternalUsers/utils/table_user_to_user.js
+++ public/apps/configuration-react/pages/InternalUsers/containers/InternalUsers/utils/table_user_to_user.js
@@ -1,9 +1,8 @@
-import { omit, uniq } from 'lodash';
+import { omit } from 'lodash';
 import { DEFAULT_PASSWORD } from '../../../../../utils/constants';
 
 const tableUserToUser = user => ({
   ...omit(user, ['id', 'hidden', 'reserved', 'static']),
-  backend_roles: uniq(user.backend_roles),
   password: DEFAULT_PASSWORD // API prohibits saving user with empty password
 });
 
