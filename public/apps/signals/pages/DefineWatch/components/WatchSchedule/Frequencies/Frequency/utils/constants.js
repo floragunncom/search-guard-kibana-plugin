0fcdcf3197a34de5edb4bfbda984bb6c2a3bab23
Mikael Gustavsson <micael.gustafsson@gmail.com>
2019-12-07 21:46:44 +0100

    LRT-592 - Added hourly trigger
    LRT-592 - Added hourly trigger
    
    LRT-592 - Added hourly trigger

--- public/apps/signals/pages/DefineWatch/components/WatchSchedule/Frequencies/Frequency/utils/constants.js
+++ public/apps/signals/pages/DefineWatch/components/WatchSchedule/Frequencies/Frequency/utils/constants.js
@@ -1,5 +1,6 @@
 export const FREQUENCIES = [
   { value: 'interval', text: 'By interval' },
+  { value: 'hourly', text: 'Hourly' },
   { value: 'daily', text: 'Daily' },
   { value: 'weekly', text: 'Weekly' },
   { value: 'monthly', text: 'Monthly' },
