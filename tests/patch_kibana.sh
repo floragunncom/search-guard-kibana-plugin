029cfd06a917e2014ca7a4e23198e3aa1c7c45ed
Sergii Bondarenko <sergibondarenko@gmail.com>
2020-02-14 18:35:36 +0100

    Update script to patch Kibana to accept any Elasticsearch version

--- tests/patch_kibana.sh
+++ tests/patch_kibana.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/bash
+#!/bin/bash
 echo "Replacing error with log message about Kibana and Elasticsearch versions incompatibility in Kibana source code"
-sed -i.bak 's/throw new Error/console.log/' ../../src/legacy/core_plugins/elasticsearch/lib/ensure_es_version.js
+sed -i.bak 's/isCompatible: ignoreVersionMismatch || incompatibleNodes.length === 0/isCompatible: true/' ../../src/core/server/elasticsearch/version_check/ensure_es_version.ts
