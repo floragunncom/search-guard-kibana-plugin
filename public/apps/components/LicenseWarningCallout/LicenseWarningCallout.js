5850e953d55bfc48116d933590b7b11d2af2eb0e
Sergii Bondarenko <sergibondarenko@gmail.com>
2020-03-13 16:50:48 +0100

    ITT-2458/ITT-2477 Refactor license warning to react component.
    (cherry picked from commit 5ec5eddf77414e90b29ea48fdbea2c4372425ac9)

---
+++ public/apps/components/LicenseWarningCallout/LicenseWarningCallout.js
@@ -0,0 +1,101 @@
+/* eslint-disable @kbn/eslint/require-license-header */
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { EuiCallOut, EuiText } from '@elastic/eui';
+import { SystemStateService } from '../../../services';
+
+export function LicenseWarningCallout({ httpClient, errorMessage }) {
+  const service = new SystemStateService(httpClient);
+
+  const [licenseValid, setLicenseValid] = useState(true);
+  const [error, setError] = useState(null);
+  const [warning, setWarning] = useState(null);
+
+  useState(() => {
+    verifyLicense();
+  }, []);
+
+  async function verifyLicense() {
+    const licenseCantBeLoadedText =
+      'The Search Guard license information could not be loaded. Please contact your system administrator.';
+    const licenseKeyIsInvalidText =
+      'The Search Guard license key is not valid for this cluster. Please contact your system administrator.';
+
+    try {
+      await service.loadSystemInfo();
+
+      if (!service.stateLoaded()) {
+        setError(licenseCantBeLoadedText);
+        setLicenseValid(false);
+        return;
+      }
+
+      if (!service.licenseRequired()) {
+        setLicenseValid(true);
+        return;
+      }
+
+      setLicenseValid(service.licenseValid());
+
+      if (errorMessage) {
+        setError(errorMessage);
+      } else {
+        setError(licenseKeyIsInvalidText);
+      }
+
+      if (service.licenseValid()) {
+        if (service.isTrialLicense() && service.expiresIn() <= 10) {
+          setWarning(`Your trial license expires in ${service.expiresIn()} days.`);
+        }
+        if (!service.isTrialLicense() && service.expiresIn() <= 20) {
+          setWarning(`Your license expires in ${service.expiresIn()} days.`);
+        }
+      }
+    } catch (error) {
+      setLicenseValid(false);
+      setError(licenseCantBeLoadedText);
+      console.error('LicenseWarning - checkLicense()', error);
+    }
+  }
+
+  return (
+    <>
+      {!licenseValid && (
+        <EuiCallOut
+          id="sg.label.licensewarning"
+          data-test-subj="sg.callout.licenseWarning"
+          title="Error"
+          color="danger"
+          iconType="alert"
+        >
+          <EuiText>
+            <p>{error}</p>
+          </EuiText>
+        </EuiCallOut>
+      )}
+
+      {warning && (
+        <EuiCallOut
+          id="sg.label.licensehint"
+          data-test-subj="sg.callout.licenseHint"
+          title="Warning"
+          color="warning"
+          iconType="help"
+        >
+          <EuiText>
+            <p>{warning}</p>
+          </EuiText>
+        </EuiCallOut>
+      )}
+    </>
+  );
+}
+
+LicenseWarningCallout.defaultProps = {
+  errorMessage: '',
+};
+
+LicenseWarningCallout.propTypes = {
+  httpClient: PropTypes.func.isRequired,
+  errorMessage: PropTypes.string.isRequired,
+};
--- public/directives/licensewarning.js
+++ public/directives/licensewarning.js
@@ -1,62 +1,22 @@
+/* eslint-disable @kbn/eslint/require-license-header */
 import { uiModules } from 'ui/modules';
-import chrome from 'ui/chrome';
-import template from './licensewarning.html';
-import { SystemStateService } from '../services';
+import React from 'react';
+import { render } from 'react-dom';
+import { LicenseWarningCallout } from '../apps/components';
 
 const app = uiModules.get('apps/searchguard/configuration', []);
 
-app.directive('sgLicenseWarning', function ($http) {
-    console.log($http);
-    const systemStateService = new SystemStateService($http);
-
-    return {
-        restrict: 'EA',
-        scope: {
-            "errorMessage": "@errormessage",
-        },
-        template,
-
-        link: function($scope, elem, attrs) {
-
-            $scope.licensevalid = true;
-            $scope.message = "";
-
-            systemStateService.loadSystemInfo().then(function(){
-
-                if (!systemStateService.stateLoaded()) {
-                    $scope.message = "The Search Guard license information could not be loaded. Please contact your system administrator.";
-                    $scope.licensevalid = false;
-                    $scope.$apply('message');
-                    return;
-                }
-
-
-                if (!systemStateService.licenseRequired()) {
-                    $scope.licensevalid = true;
-                    return;
-                }
-
-                $scope.licensevalid = systemStateService.licenseValid();
-
-                if ($scope.errorMessage) {
-                    $scope.message = $scope.errorMessage;
-                } else {
-                    $scope.message = "The Search Guard license key is not valid for this cluster. Please contact your system administrator.";
-                }
-
-                if (systemStateService.licenseValid()) {
-                    if (systemStateService.isTrialLicense() && systemStateService.expiresIn() <= 10) {
-                        $scope.hint = "Your trial license expires in " + systemStateService.expiresIn() + " days.";
-                        $scope.$apply('hint');
-                    }
-                    if (!systemStateService.isTrialLicense() && systemStateService.expiresIn() <= 20) {
-                        $scope.hint = "Your license expires in " + systemStateService.expiresIn() + " days.";
-                        $scope.$apply('hint');
-                    }
-
-                }
-            });
-
-        }
-    }
+app.directive('sgLicenseWarning', $http => {
+  return {
+    restrict: 'EA',
+    scope: {
+      errorMessage: '@errormessage',
+    },
+    link: function(scope, el) {
+      scope.$watch('errorMessage', function(newValue) {
+        const licenseWarning = React.createFactory(LicenseWarningCallout);
+        render(licenseWarning({ errorMessage: newValue, httpClient: $http }), el[0]);
+      });
+    },
+  };
 });
