ae45b6661d8a4e9f52f5b7e44308a2f5b5d349fd
Sergii Bondarenko <sergibondarenko@gmail.com>
2020-06-17 16:25:49 +0200

    Draft for check editor in a flyout

--- public/apps/signals/pages/DefineWatch/components/BlocksWatch/forms/StaticCheckBlockForm.js
+++ public/apps/signals/pages/DefineWatch/components/BlocksWatch/forms/StaticCheckBlockForm.js
@@ -1,57 +1,132 @@
 /* eslint-disable @kbn/eslint/require-license-header */
-import React from 'react';
-import { EuiFlexGroup, EuiFlexItem, EuiSpacer } from '@elastic/eui';
-import { CheckCodeEditor } from './CheckCodeEditor';
+import React, { useContext } from 'react';
+import { EuiFlexGroup, EuiFlexItem, EuiSpacer, EuiButton, EuiCodeBlock, EuiFormRow } from '@elastic/eui';
+import { CheckCodeEditor } from './CheckCodeEditorTest';
+import { CheckResponse } from './CheckResponseTest';
 import { CheckType } from './CheckType';
 import { CheckName } from './CheckName';
 import { CheckTarget } from './CheckTarget';
-import { CheckResponse } from './CheckResponse';
 import { ResponseLabelAppend } from './ResponseLabelAppend';
 import { EDITOR_OPTIONS } from '../utils/constants';
 import { validateJsonString } from '../../../utils/validate';
-import { DOC_LINKS } from '../../../../../utils/constants';
+import { DOC_LINKS, FLYOUTS } from '../../../../../utils/constants';
+
+import { Context } from '../../../../../Context';
+
+function EditorAndResponse({ valuePath, checkBlock, onCloseResult }) {
+  return (
+    <EuiFlexGroup>
+      <EuiFlexItem>
+        <CheckResponse value={checkBlock.value} rowProps={{ label: 'value' }} />
+      </EuiFlexItem>
+      <EuiFlexItem>
+        <CheckResponse value={checkBlock.response} rowProps={{ label: 'response' }} />
+      </EuiFlexItem>
+    </EuiFlexGroup>
+  );
+}
+/*
+      <EuiFlexItem>
+        <CheckCodeEditor
+          formikFieldProps={{
+            validate: validateJsonString,
+          }}
+          editorProps={{
+            mode: 'json',
+            setOptions: EDITOR_OPTIONS,
+          }}
+          valuePath={valuePath}
+          docLink={DOC_LINKS.INPUTS.STATIC}
+        />
+      </EuiFlexItem>
+      {checkBlock.response && (
+        <EuiFlexItem>
+          <CheckResponse
+            rowProps={{
+              labelAppend: <ResponseLabelAppend onClick={onCloseResult} />,
+            }}
+            editorProps={{
+              setOptions: EDITOR_OPTIONS,
+            }}
+            value={checkBlock.response}
+          />
+        </EuiFlexItem>
+      )}
+*/
 
 export function StaticCheckBlockForm({ index, checkBlock, checksBlocksPath, onCloseResult }) {
+  const { triggerFlyout } = useContext(Context);
+
   const typePath = `${checksBlocksPath}[${index}].type`;
   const namePath = `${checksBlocksPath}[${index}].name`;
   const targetPath = `${checksBlocksPath}[${index}].target`;
   const valuePath = `${checksBlocksPath}[${index}].value`;
+
+  function openEditor() {
+    triggerFlyout({
+      type: FLYOUTS.CUSTOM,
+      payload: {
+        flyoutProps: { size: 'l' },
+        body: (
+          <EditorAndResponse
+            valuePath={valuePath}
+            checkBlock={checkBlock}
+            onCloseResult={() => onCloseResult(index)}
+          />
+        ),
+      },
+    });
+  }
 
   return (
     <>
       <CheckType typePath={typePath} />
       <CheckName namePath={namePath} />
       <CheckTarget targetPath={targetPath} />
-      <EuiSpacer />
 
-      <EuiFlexGroup>
-        <EuiFlexItem>
-          <CheckCodeEditor
-            formikFieldProps={{
-              validate: validateJsonString,
-            }}
-            editorProps={{
-              mode: 'json',
-              setOptions: EDITOR_OPTIONS,
-            }}
-            valuePath={valuePath}
-            docLink={DOC_LINKS.INPUTS.STATIC}
-          />
-        </EuiFlexItem>
-        {checkBlock.response && (
-          <EuiFlexItem>
-            <CheckResponse
-              rowProps={{
-                labelAppend: <ResponseLabelAppend onClick={() => onCloseResult(index)} />,
-              }}
-              editorProps={{
-                setOptions: EDITOR_OPTIONS,
-              }}
-              value={checkBlock.response}
-            />
-          </EuiFlexItem>
-        )}
-      </EuiFlexGroup>
+      <EuiSpacer />
+      <EuiFormRow label="Value" helpText="Click on the code block to edit.">
+        <div onClick={openEditor}>
+          <EuiCodeBlock>{checkBlock.value}</EuiCodeBlock>
+        </div>
+      </EuiFormRow>
     </>
   );
+  // return (
+  //   <>
+  //     <CheckType typePath={typePath} />
+  //     <CheckName namePath={namePath} />
+  //     <CheckTarget targetPath={targetPath} />
+  //     <EuiSpacer />
+
+  //     <EuiFlexGroup>
+  //       <EuiFlexItem>
+  //         <CheckCodeEditor
+  //           formikFieldProps={{
+  //             validate: validateJsonString,
+  //           }}
+  //           editorProps={{
+  //             mode: 'json',
+  //             setOptions: EDITOR_OPTIONS,
+  //           }}
+  //           valuePath={valuePath}
+  //           docLink={DOC_LINKS.INPUTS.STATIC}
+  //         />
+  //       </EuiFlexItem>
+  //       {checkBlock.response && (
+  //         <EuiFlexItem>
+  //           <CheckResponse
+  //             rowProps={{
+  //               labelAppend: <ResponseLabelAppend onClick={() => onCloseResult(index)} />,
+  //             }}
+  //             editorProps={{
+  //               setOptions: EDITOR_OPTIONS,
+  //             }}
+  //             value={checkBlock.response}
+  //           />
+  //         </EuiFlexItem>
+  //       )}
+  //     </EuiFlexGroup>
+  //   </>
+  // );
 }
