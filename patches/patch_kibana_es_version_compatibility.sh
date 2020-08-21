6d4cc7305a407dd709d017ad947e76bbf249cba5
Sergii Bondarenko <sergibondarenko@gmail.com>
2020-08-21 16:17:05 +0200

    Make Kibana version compatibility patch prod ready

--- patches/patch_kibana_es_version_compatibility.sh
+++ patches/patch_kibana_es_version_compatibility.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
 
+COMMAND="$1"
+set -f
 set -e
+
+if [ -z "$COMMAND" ]; then
+  COMMAND="prod"
+fi
+
+if [[ "$COMMAND" != "prod" && "$COMMAND" != "dev" ]]; then
+  echo "Unknown command: $COMMAND"
+  echo
+  echo "Usage:"
+  echo "./patch.sh               Patch Kibana in a production environment."
+  echo "./patch.sh dev           Patch Kibana in a development environment."
+  exit 1
+fi
+
+EXT="js"
+if [[ "$COMMAND" == "dev" ]]; then
+  EXT="ts"
+fi
 
 echo
 echo "Patching Kibana and Elasticsearch versions incompatibility in Kibana source code"
-echo "ATTENTION! It is only for the developers! It doesn't work in a production environment."
 echo
 
-FILE_PATH="../../src/core/server/elasticsearch/version_check/ensure_es_version.ts"
+FILE_PATH="../../src/core/server/elasticsearch/version_check/ensure_es_version.$EXT"
 sed -i.bak 's/isCompatible: ignoreVersionMismatch || incompatibleNodes.length === 0/isCompatible: true/' $FILE_PATH 
 
 echo "Patched $FILE_PATH. The original file backup is in $FILE_PATH.bak"
