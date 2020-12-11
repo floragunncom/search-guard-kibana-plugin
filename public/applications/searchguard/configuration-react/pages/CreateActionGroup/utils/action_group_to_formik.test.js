3b27e9b137475d964947437980cfec698627af27
Sergii Bondarenko <sergibondarenko@gmail.com>
2020-12-11 17:34:50 +0100

    Lint the code.

--- public/applications/searchguard/configuration-react/pages/CreateActionGroup/utils/action_group_to_formik.test.js
+++ public/applications/searchguard/configuration-react/pages/CreateActionGroup/utils/action_group_to_formik.test.js
@@ -1,44 +1,45 @@
+/*
+ *    Copyright 2020 floragunn GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import { actionGroupsToUiActionGroups, actionGroupToFormik } from './action_group_to_formik';
 
 describe('action group to UI action group', () => {
   test('can build UI action groups', () => {
     const resource = {
-      'B': {},
-      'A': {},
-      'C': {}
+      B: {},
+      A: {},
+      C: {},
     };
 
-    const uiResource = [
-      { label: 'B' },
-      { label: 'C' }
-    ];
+    const uiResource = [{ label: 'B' }, { label: 'C' }];
 
     expect(actionGroupsToUiActionGroups(resource, ['A'])).toEqual(uiResource);
   });
 
   test('can build UI action group', () => {
     const resource = {
-      allowed_actions: [
-        'kibana:a/b/c',
-        'cluster:a/b/c',
-        'indices:a/b/c',
-        'B',
-        'A'
-      ],
+      allowed_actions: ['kibana:a/b/c', 'cluster:a/b/c', 'indices:a/b/c', 'B', 'A'],
       type: 'cluster',
       hidden: false,
       reserved: false,
-      static: false
+      static: false,
     };
 
     const uiResource = {
-      allowed_actions: [
-        'kibana:a/b/c',
-        'cluster:a/b/c',
-        'indices:a/b/c',
-        'B',
-        'A'
-      ],
+      allowed_actions: ['kibana:a/b/c', 'cluster:a/b/c', 'indices:a/b/c', 'B', 'A'],
       type: 'cluster',
       hidden: false,
       reserved: false,
@@ -48,12 +49,9 @@ describe('action group to UI action group', () => {
       _permissions: [
         { label: 'cluster:a/b/c' },
         { label: 'indices:a/b/c' },
-        { label: 'kibana:a/b/c' }
+        { label: 'kibana:a/b/c' },
       ],
-      _actiongroups: [
-        { label: 'A' },
-        { label: 'B' }
-      ]
+      _actiongroups: [{ label: 'A' }, { label: 'B' }],
     };
 
     expect(actionGroupToFormik(resource, uiResource._name)).toEqual(uiResource);
--- public/applications/searchguard/configuration-react/pages/CreateActionGroup/utils/formik_to_action_group.test.js
+++ public/applications/searchguard/configuration-react/pages/CreateActionGroup/utils/formik_to_action_group.test.js
@@ -1,26 +1,30 @@
+/*
+ *    Copyright 2020 floragunn GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import formikToActionGroup from './formik_to_action_group';
 
 describe('UI action group to action group', () => {
   test('can build action group', () => {
     const resource = {
-      allowed_actions: [
-        'cluster:a/b/c',
-        'indices:a/b/c',
-        'kibana:a/b/c',
-        'A',
-        'B'
-      ],
-      type: 'cluster'
+      allowed_actions: ['cluster:a/b/c', 'indices:a/b/c', 'kibana:a/b/c', 'A', 'B'],
+      type: 'cluster',
     };
 
     const uiResource = {
-      allowed_actions: [
-        'kibana:a/b/c',
-        'cluster:a/b/c',
-        'indices:a/b/c',
-        'B',
-        'A'
-      ],
+      allowed_actions: ['kibana:a/b/c', 'cluster:a/b/c', 'indices:a/b/c', 'B', 'A'],
       type: 'cluster',
       hidden: false,
       reserved: false,
@@ -30,12 +34,9 @@ describe('UI action group to action group', () => {
       _permissions: [
         { label: 'cluster:a/b/c' },
         { label: 'indices:a/b/c' },
-        { label: 'kibana:a/b/c' }
+        { label: 'kibana:a/b/c' },
       ],
-      _actiongroups: [
-        { label: 'A' },
-        { label: 'B' }
-      ]
+      _actiongroups: [{ label: 'A' }, { label: 'B' }],
     };
 
     expect(formikToActionGroup(uiResource)).toEqual(resource);
