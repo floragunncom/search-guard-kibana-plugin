dca894aea17dd9dea44e7cad6cfcb1367cbd494f
Nils Bandener <git@bndnr.de>
2021-09-15 16:26:16 +0200

    Deleted obsolete install demo configuration script
    Now replaced by integrated demo installation script in backend repo
    
    Conflicts:
    	install_demo_configuration.js

--- install_demo_configuration.ps1
+++
@@ -1,7 +0,0 @@
-$NODE_PATH = ".\..\..\node\nnode.exe" # The Kibana prod built-in Node.js
-if(!(Test-Path $NODE_PATH])) {
-    $NODE_PATH = (gcm node.exe).Path # Find Node.js on the system
-}
-
-$ARG = "install_demo_configuration.js"
-& $NODE_PATH $ARG
