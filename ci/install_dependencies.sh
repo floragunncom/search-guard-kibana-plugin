b03d7f13114e3c9c83c7c4bc448e1a2a1a86675f
Nils Bandener <9-nibix@users.noreply.git.floragunn.com>
2021-09-24 06:26:33 +0000

    New build CI for Kibana/Dashboards
    See merge request !751

---
+++ ci/install_dependencies.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+source ci/utils.sh
+
+start_collapsed_section nvm "Installing NVM and Node"
+
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.34.0/install.sh| bash
+export NVM_HOME=~/.nvm
+export NVM_DIR="$HOME/.nvm"
+
+[ -s "$NVM_DIR/nvm.sh" ]; \. "$NVM_DIR/nvm.sh"
+
+nvm install $NODE_VERSION
+
+end_section nvm
+
+
+
+start_collapsed_section yarn_install "Installing Yarn"
+
+export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
+
+if ! [ -x "$(command -v yarn)" ]; then
+  echo "+++ Installing Yarn +++"
+  curl -Ss -o- -L https://yarnpkg.com/install.sh | bash 
+else
+  echo "    -> $(yarn -v)"
+fi
+
+end_section "yarn_install"
