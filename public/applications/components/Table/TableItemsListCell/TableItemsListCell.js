f561b970e17f076fb1a5a6531435392341bbe25c
Nils Bandener <git@bndnr.de>
2022-04-13 20:02:37 +0200

    Made Search Guard config UI tolerant of absent JSON properties

--- public/applications/components/Table/TableItemsListCell/TableItemsListCell.js
+++ public/applications/components/Table/TableItemsListCell/TableItemsListCell.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const TableItemsListCell = ({ items, name }) => (
+const TableItemsListCell = ({ items = [], name }) => (
   <div data-test-subj={`sgTableCol-${name}`}>
     {items.map((item, i) => <div key={i}>{item}</div>)}
   </div>
