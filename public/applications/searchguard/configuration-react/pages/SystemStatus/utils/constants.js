dd174bf444ce8c9119d652cfd3a57d437b123d52
Nils Bandener <git@bndnr.de>
2022-04-27 13:23:38 +0200

    Removed remaining pieces of active modules

--- public/applications/searchguard/configuration-react/pages/SystemStatus/utils/constants.js
+++ public/applications/searchguard/configuration-react/pages/SystemStatus/utils/constants.js
@@ -2,7 +2,6 @@ export const SIDE_NAV = {
   SYSTEM_STATUS: 'systemStatus',
   CLUSTER: 'cluster',
   LICENSE: 'license',
-  ACTIVE_MODULES: 'activeModules'
 };
 
 export const SELECTED_SIDE_NAV_ITEM_NAME = SIDE_NAV.CLUSTER;
