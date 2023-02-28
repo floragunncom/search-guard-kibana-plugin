b013564c41e9608da62eb4e3e23b760153b78902
Nils Bandener <nils.bandener@eliatra.com>
2023-02-28 09:01:38 +0000

    Adapted release tag schema
    Closes #9
    
    See merge request eliatra-suite/eliatra-dashboards-plugin!9

--- ci/setup_gpg.sh
+++ ci/setup_gpg.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo -n "$PKEY" | base64 --decode > private.key
-
 eval `gpg-agent --daemon --pinentry-program /usr/bin/pinentry-tty --no-grab` > /dev/null 2>&1
 export GPG_TTY=`tty`
 export GPG_AGENT_INFO
@@ -21,9 +19,9 @@ export GPG_TTY=`tty`
 export GPG_AGENT_INFO
 export PINENTRY_USER_DATA="tty"
 
-echo "$PASS" | gpg --pinentry-mode loopback --passphrase-fd 0 --allow-secret-key-import --import private.key > /dev/null 2>&1 || true
+cat "$CODE_SIGNING_KEY_PASS" | gpg --pinentry-mode loopback --passphrase-fd 0 --allow-secret-key-import --import "$CODE_SIGNING_KEY" > /dev/null 2>&1 || true
 expect -c "spawn gpg --edit-key 4A61C8AE trust quit; send \"5\ry\r\"; expect eof" > /dev/null 2>&1 || true
 
 #cache key
 echo "123" > cl.txt
-echo "$PASS" | gpg --pinentry-mode loopback --passphrase-fd 0 --textmode --always-trust --default-key 4A61C8AE -z 0 -a --clearsign cl.txt > /dev/null 2>&1
+cat "$CODE_SIGNING_KEY_PASS" | gpg --pinentry-mode loopback --passphrase-fd 0 --textmode --always-trust --default-key 4A61C8AE -z 0 -a --clearsign cl.txt > /dev/null 2>&1
