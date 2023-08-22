1dcf46bd54e8ec02e8c945d6dd29a44538893519
Jens Rode <derjens@fastmail.de>
2023-08-22 15:54:34 +0200

    Updated the corresponding unit tests

--- tests/jest.config.js
+++ tests/jest.config.js
@@ -2,11 +2,11 @@
 module.exports = {
   preset: '@kbn/test/jest_node',
   rootDir: '../../..',
-  roots: ['<rootDir>/plugins/search-guard'],
+  roots: ['<rootDir>/plugins/search-guard-kibana-plugin'],
   moduleNameMapper: {
-	// XXX: We should not use moduleNameMapper due to its performance impact. We should try to replace this by other solutions 
-    '!!raw-loader!./worker.js': '<rootDir>/plugins/search-guard/__mocks__/raw_loader.js',
-    'ui/chrome': '<rootDir>/plugins/search-guard/__mocks__/chrome.js',
-    '\\.svg': '<rootDir>/plugins/search-guard/__mocks__/svg_mock.js',
+	// XXX: We should not use moduleNameMapper due to its performance impact. We should try to replace this by other solutions
+    '!!raw-loader!./worker.js': '<rootDir>/plugins/search-guard-kibana-plugin/__mocks__/raw_loader.js',
+    'ui/chrome': '<rootDir>/plugins/search-guard-kibana-plugin/__mocks__/chrome.js',
+    '\\.svg': '<rootDir>/plugins/search-guard-kibana-plugin/__mocks__/svg_mock.js',
   },
 }
