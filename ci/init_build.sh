4ef2816b9e8348e204b13e15d45442b001b79723
Jens Rode <derjens@fastmail.de>
2023-11-22 11:15:44 +0100

    Even more debugging

--- ci/init_build.sh
+++ ci/init_build.sh
@@ -58,6 +58,9 @@ echo -e "\e[0Ksection_end:`date +%s`:yarn_bootstrap\r\e[0K"
 
 #end_section replace_kbn_ui_shared_deps_npm_manifest_json
 
+# Debugging
+rm yarn.lock
+
 mkdir -p plugins/search-guard
 cp -a "../babel.config.js" plugins/search-guard
 cp -a "../package.json" plugins/search-guard
@@ -68,10 +71,7 @@ cp -a "../server" plugins/search-guard
 cp -a "../common" plugins/search-guard
 cp -a "../tests"  plugins/search-guard
 cp -a "../__mocks__" plugins/search-guard
-#cp -a "../yarn.lock" plugins/search-guard
-
-# Prevent warning about outdated caniuse-lite, which seems to block the build
-npx --yes update-browserslist-db@latest
+cp -a "../yarn.lock" plugins/search-guard
 
 cd plugins/search-guard
 
