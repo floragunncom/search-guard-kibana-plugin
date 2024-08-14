e66445d7431a2a8cdb2f14090611905654e2a903
Jens Rode <derjens@fastmail.de>
2024-08-14 08:34:16 +0000

    Bump to ES 8.14.3

--- public/applications/searchguard/configuration-react/components/Icon/Icon.js
+++ public/applications/searchguard/configuration-react/components/Icon/Icon.js
@@ -16,7 +16,9 @@ const Icon = ({ type, size = 'xl' }) => {
   const { configService } = useContext(Context);
 
   const _size = isObject(size) ? size : sizes[size];
-  const _type = configService.get('is_dark_mode') ? type + '_dark' : type;
+  // TODO Check UIConfigService.js for a comment related to this.
+  // At the moment, dark mode isn't really implemented.
+  const _type = configService.get('is_dark_mode') === true ? type + '_dark' : type;
   const Svg = icons[_type] || icons.empty;
   return <Svg {..._size} />;
 };
