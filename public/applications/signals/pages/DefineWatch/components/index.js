7772ca21642469090bc4814341a304c3bc12d321
Jens Rode <derjens@fastmail.de>
2024-11-11 11:42:24 +0100

    Finally fix the build warning about a missing component

--- public/applications/signals/pages/DefineWatch/components/index.js
+++ public/applications/signals/pages/DefineWatch/components/index.js
@@ -1,3 +1,3 @@
 export { default as GeneralPanel } from './GeneralPanel';
 export { default as DefinitionPanel } from './DefinitionPanel';
-export { ActionPanel, ResolveActionPanel } from './ActionPanel';
+export { ActionPanel } from './ActionPanel';
