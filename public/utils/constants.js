c94c3d5449cca1d602e215efecf8081435b6dced
Jens Rode <derjens@fastmail.de>
2025-07-25 12:49:52 +0200

    #501 - Updated SG logo

--- public/utils/constants.js
+++ public/utils/constants.js
@@ -1,12 +1,22 @@
 /* eslint-disable @kbn/eslint/require-license-header */
 import sgLogo from '../assets/searchguard_logo_left_navbar.svg';
+import sgLogoDark from '../assets/searchguard_logo_left_navbar_dark.svg';
+
 
 export const PLUGIN_NAME = 'searchguard';
 export const API_ROOT = '/api/v1';
 
-export const SEARCHGUARD_APP_CATEGORY = {
-  id: 'searchguard-configuration-cat',
-  label: 'Search Guard',
-  euiIconType: sgLogo,
-  order: 9010,
-};
+/**
+ *
+ * @param configService
+ * @returns {{id: string, label: string, euiIconType: {}, order: number}}
+ */
+export const getSearchGuardAppCategory = (configService) => {
+  const isDarkMode = (configService && configService.get('is_dark_mode')) ? true : false;
+  return {
+    id: 'searchguard-configuration-cat',
+    label: 'Search Guard',
+    euiIconType: isDarkMode ? sgLogoDark: sgLogo,
+    order: 9010,
+  }
+}
