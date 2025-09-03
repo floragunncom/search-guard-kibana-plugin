75b6436acd5d3f14d8a959785c4ca80152b52328
DevOps Automat <devops@automat.berlin>
2025-09-03 15:41:00 +0000

    Remove Basel and adapt to run as kibana user
    See merge request search-guard/search-guard-kibana-plugin!1076

--- ci/init_build.sh
+++ ci/init_build.sh
@@ -82,26 +82,10 @@ fi
 npx update-browserslist-db@latest 
 
 
-yarn kbn bootstrap --allow-root
+yarn kbn bootstrap
 
 echo -e "\e[0Ksection_end:`date +%s`:yarn_bootstrap\r\e[0K"
 
-# Not needed for 8.19.3+
-#start_section replace_kbn_ui_shared_deps_npm_manifest_json "Replace bazel-bin/../kbn-ui-shared-deps-npm-manifest.json with file from kibana release"
-#
-#echo "Will download $SF_RELEASE_PACKAGE_URL"
-#
-#curl --fail $SF_RELEASE_PACKAGE_URL -o kibana-$SF_VERSION.tar.gz
-#tar -xf kibana-$SF_VERSION.tar.gz --transform 's/.*\///g' --wildcards --no-anchored 'kbn-ui-shared-deps-npm-manifest.json'
-#
-#if [ ! -f bazel-bin/src/platform/packages/private/kbn-ui-shared-deps-npm/shared_built_assets/kbn-ui-shared-deps-npm-manifest.json ]; then
-#  echo "File bazel-bin/src/platform/packages/kbn-ui-shared-deps-npm/shared_built_assets/kbn-ui-shared-deps-npm-manifest.json not found"
-#  exit 1
-#fi
-#mv kbn-ui-shared-deps-npm-manifest.json bazel-bin/src/platform/packages/private/kbn-ui-shared-deps-npm/shared_built_assets/kbn-ui-shared-deps-npm-manifest.json
-#rm -rf kibana-$SF_VERSION.tar.gz
-#
-#end_section replace_kbn_ui_shared_deps_npm_manifest_json
 
 mkdir -p plugins/search-guard
 cp -a "../babel.config.js" plugins/search-guard
