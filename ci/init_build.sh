e63c72d8ac4dfc9530473ea5600220e15a0ef182
Piotr Chmielnik <piotr.chmielnik@eliatra.com>
2025-10-22 00:18:16 +0200

    Fix

--- ci/init_build.sh
+++ ci/init_build.sh
@@ -76,10 +76,11 @@ fi
 echo -e "\e[0Ksection_start:`date +%s`:yarn_bootstrap[collapsed=true]\r\e[0KDoing yarn bootstrap"
 
 if grep -q '"@elastic/eui@104.0.0-amsterdam.0"' yarn.lock; then
-    echo "Install eui@104.0.0-amsterdam.0 manually"
+   #  echo "Install eui@104.0.0-amsterdam.0 manually"
+    echo "Remove eui@104.0.0-amsterdam.0"
     yarn remove @elastic/eui
-    curl -L --fail "https://registry.yarnpkg.com/@elastic/eui/-/eui-104.0.0-amsterdam.0.tgz" -o "/tmp/eui-104.0.0-amsterdam.0.tgz"
-    yarn add "file:/tmp/eui-104.0.0-amsterdam.0.tgz" --force
+   #  curl -L --fail "https://registry.yarnpkg.com/@elastic/eui/-/eui-104.0.0-amsterdam.0.tgz" -o "/tmp/eui-104.0.0-amsterdam.0.tgz"
+   #  yarn add "file:/tmp/eui-104.0.0-amsterdam.0.tgz" --force
 fi
 
 echo "DEBUG yarn.lock"
