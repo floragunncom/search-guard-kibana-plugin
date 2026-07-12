42bfb4283d9f07a3889168c61d9a519087318283
Hendrik Saly <hsaly@search-guard.com>
2026-07-12 20:27:14 +0200

    remove unused stuff, try to reuse init_build.sh for local build

--- ci/init_build.sh
+++ ci/init_build.sh
@@ -2,8 +2,27 @@
 
 set -e
 
+# Resolve the project root. In CI it is provided via CI_PROJECT_DIR, otherwise
+# default to the root folder of the project (the parent of this ci/ directory).
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PROJECT_DIR="${CI_PROJECT_DIR:-$( cd "$SCRIPT_DIR/.." && pwd )}"
+
+echo "CI_PROJECT_DIR: $CI_PROJECT_DIR"
+echo "PROJECT_DIR: $PROJECT_DIR"
+
+# Load shared helpers (start_section/end_section/...).
+source "$SCRIPT_DIR/utils.sh"
+
+# Use GNU sed. macOS ships BSD sed, which is incompatible with the in-place
+# edits below, so prefer gsed (brew install gnu-sed) when it is available.
+if command -v gsed >/dev/null 2>&1; then
+   SED="gsed"
+else
+   SED="sed"
+fi
+
 SF_BRANCH_NAME="v$SF_VERSION"
-SF_RELEASE_PACKAGE_URL="https://artifacts.elastic.co/downloads/kibana/kibana-$SF_VERSION-linux-x86_64.tar.gz"
+#SF_RELEASE_PACKAGE_URL="https://artifacts.elastic.co/downloads/kibana/kibana-$SF_VERSION-linux-x86_64.tar.gz"
 
 if [[ -f $SF_REPO_DIR/.cached_version ]]; then
    CACHED_VERSION=`cat $SF_REPO_DIR/.cached_version`
@@ -38,12 +57,15 @@ fi
 
 echo -e "\e[0Ksection_start:`date +%s`:patch_kbn_optimizer[collapsed=true]\r\e[0KPatch kbn optimizer"
 
-sed -i "/observeLines(proc.stderr\!).pipe(Rx.map((line) => ({ type: 'stderr', data: line }))),/s/^/\/\//" packages/kbn-plugin-helpers/src/tasks/optimize.ts
+$SED -i "/observeLines(proc.stderr\!).pipe(Rx.map((line) => ({ type: 'stderr', data: line }))),/s/^/\/\//" packages/kbn-plugin-helpers/src/tasks/optimize.ts
 
 echo -e "\e[0Ksection_end:`date +%s`:patch_kbn_optimizer\r\e[0K"
 
 
 echo -e "\e[0Ksection_start:`date +%s`:nvm_install[collapsed=true]\r\e[0KDoing nvm install"
+
+# Ensure nvm is loaded (it may not be when this script is run standalone).
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
 nvm install
 
@@ -113,9 +135,9 @@ echo -e "\e[0Ksection_end:`date +%s`:yarn_install\r\e[0K"
 
 start_section tests "Unit Tests"
 
-export JEST_JUNIT_OUTPUT_FILE=$CI_PROJECT_DIR/junit-server.xml
+export JEST_JUNIT_OUTPUT_FILE=$PROJECT_DIR/junit-server.xml
 ../../node_modules/.bin/jest --clearCache && ../../node_modules/.bin/jest --testPathIgnorePatterns=server --config ./tests/jest.config.js --reporters="default"
-export JEST_JUNIT_OUTPUT_FILE=$CI_PROJECT_DIR/junit-public.xml
+export JEST_JUNIT_OUTPUT_FILE=$PROJECT_DIR/junit-public.xml
 ../../node_modules/.bin/jest --clearCache && ../../node_modules/.bin/jest --testPathIgnorePatterns=public  --config ./tests/jest.config.js --reporters="default"
 end_section tests
 rm -rf "node_modules"
@@ -132,17 +154,17 @@ yarn build -v $SF_VERSION --skip-archive
 
 # Fix only for Kibana 8.7.x
 cd build
-find ./ -type f -exec sed -i -e 's#".*core-doc-links-server-internal"#"@kbn/core-doc-links-server-internal"#g' {} \;
-find ./ -type f -exec sed -i -e 's#".*packages/kbn-config-schema"#"@kbn/config-schema"#g' {} \;
-find ./ -type f -exec sed -i -e 's#".*core-saved-objects-migration-server-internal"#"@kbn/core-saved-objects-migration-server-internal"#g' {} \;
-find ./ -type f -exec sed -i -e 's#".*core-http-router-server-internal"#"@kbn/core-http-router-server-internal"#g' {} \;
-find ./ -type f -exec sed -i -e 's#".*core-http-server"#"@kbn/core-http-server"#g' {} \;
+find ./ -type f -exec $SED -i -e 's#".*core-doc-links-server-internal"#"@kbn/core-doc-links-server-internal"#g' {} \;
+find ./ -type f -exec $SED -i -e 's#".*packages/kbn-config-schema"#"@kbn/config-schema"#g' {} \;
+find ./ -type f -exec $SED -i -e 's#".*core-saved-objects-migration-server-internal"#"@kbn/core-saved-objects-migration-server-internal"#g' {} \;
+find ./ -type f -exec $SED -i -e 's#".*core-http-router-server-internal"#"@kbn/core-http-router-server-internal"#g' {} \;
+find ./ -type f -exec $SED -i -e 's#".*core-http-server"#"@kbn/core-http-server"#g' {} \;
 cd ..
 
 end_section yarn_build
 end_section build
-# Move build result from repo dir to the build folder in the CI root dir.
-cd $CI_PROJECT_DIR
+# Move build result from repo dir to the build folder in the project root dir.
+cd $PROJECT_DIR
 rm -rf build
 mv $SF_REPO_DIR/plugins/search-guard/build build
 # Remove search-guard dir from repo to have a clean repo for the Gitlab CI cache
