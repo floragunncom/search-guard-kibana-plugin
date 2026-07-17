d792f9a9ee8b8b95d94dc8b080b72d6484978b4e
Hendrik Saly <hsaly@search-guard.com>
2026-07-17 15:51:42 +0200

    CI and Build Improvements
    See merge request search-guard/search-guard-kibana-plugin!1191

--- clean.sh
+++ clean.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd "$SCRIPT_DIR"
 set -e
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $DIR
 find . -name '.DS_Store' -type f -delete
 rm -rf ./target
 rm -rf ./releases
@@ -9,3 +9,14 @@ rm -rf ./node_modules
 rm -rf ./build
 rm -f *.log
 rm -rf build_stage/kibana/plugins/* || true
+
+rm -rf ./kibana/target
+rm -rf ./kibana/releases
+rm -rf ./kibana/node_modules
+rm -rf ./kibana/build
+rm -rf ./kibana/.buildkite/node_modules/
+rm -rf ./kibana/.moon/cache/
+rm -rf ./kibana/.yarn-local-mirror/
+rm -rf ./kibana/data/
+rm -rf ./kibana/packages/kbn-ts-projects/config-paths.json
+rm -rf ./kibana/src/platform/packages/private/kbn-repo-packages/package-map.json
